d49bb1ff05d48e85f1de4ad2a96001a45aa087c6
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-09-27 01:13:44 -0300

    adicionando comentarios

--- src/app/Moment.ts
+++ src/app/Moment.ts
@@ -5,5 +5,5 @@ export interface Moment {
     image: string;
     created_at?: string;
     updated_at?: string;
-    comments?: [{ text: string; user: string }];
+    comments?: [{ text: string; username: string }];
 }

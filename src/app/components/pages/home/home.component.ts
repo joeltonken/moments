1b8dd3341e07baea2277ba1b006984728dbfaf97
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-09-26 22:55:20 -0300

    busca de dados

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -19,6 +19,9 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
+  faSearch = faSearch;
+  searchTerm: string = '';
+
   constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
@@ -34,6 +37,15 @@ export class HomeComponent implements OnInit {
 
       this.allMoments = data;
       this.moments = data;
+    });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value);
     });
   }
 }

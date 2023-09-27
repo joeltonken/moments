23c98b69c5a6d8cd82bfc04b4c2248fab62ab386
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-09-27 00:36:46 -0300

    finalizando update

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -15,7 +16,12 @@ export class EditMomentComponent implements OnInit {
   moment!: Moment;
   btnText: string = 'Editar';
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) {}
+  constructor(
+    private momentService: MomentService, 
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
+    ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -25,4 +31,24 @@ export class EditMomentComponent implements OnInit {
     })
   }
 
+  async editHandler(momentData: Moment) {
+
+    const id = this.moment.id;
+    
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+
+    if(momentData.image) {
+      formData.append('image', momentData.image);
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messagesService.add(`Moment ${id} foi atualizado com sucesso!`);
+
+    this.router.navigate(['/']);
+
+  }
 }

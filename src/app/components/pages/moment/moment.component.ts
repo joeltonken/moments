36a065f494c520bc52b34fdb2b3db550d843890c
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-09-26 23:54:14 -0300

    excluindo registros

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 import { Moment } from 'src/app/Moment';
 
@@ -23,7 +24,9 @@ export class MomentComponent implements OnInit {
 
   constructor(
       private momentService: MomentService, 
-      private route: ActivatedRoute
+      private route: ActivatedRoute,
+      private messagesService: MessagesService,
+      private router: Router
     ) {}
 
   ngOnInit(): void {
@@ -32,5 +35,13 @@ export class MomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((item) => (this.moment = item.data));
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe();
+
+    this.messagesService.add('Momento excluido com sucesso!');
+
+    this.router.navigate(['/']);
   }
 }
